797f172e4c324e7483b56badc79fd3fa386a8cd6
Jordi Bertran de Balanda <jbertran@users.noreply.github.com>
2019-09-10 22:17:21 +0200

    deploy: use PEX in deploy, update README

--- README.md
+++ README.md
@@ -2,34 +2,26 @@
 
 Keep Linux configuration files for various modules and programs for fast deployment to new machines.
 
-To deploy, just clone and run `deploy.sh`.
 
-## Directory tree
+### Deploying dotfiles
 
-* `dotfiles`
-  * `.tmux.conf`
-  *`.zlogin`
-  *`.spacemacs`
-  *`.emacs.d`
-  *`.gitconfig`
-  *`.gitignore-global`
-  *`.zshrc`
-  *`.sh_aliases`
-  *`.sh_env`
-  * `oh-my-zsh` - local clone of [oh-my-zsh clone]().
-  * `tmux-themepack` - local clone of [tmux-themepack]()
-* `home/<USER>`
-  * symlinks for config files
-  * git
-    * work
-    * perso
+Run `deploy.sh`, which orchestrates:
 
-Optional powerline fonts and terminal color themes repositories are both removed after
-their installation is complete.
+* Running the pex binary that provides the `doit` orchestration tool.
+* Installing Powerline fonts if required
+* Sourcing `~/.zshrc` for _some_ feedback on success
 
-## WiP
 
-* Package this as a .deb for more install targets and dependencies (zsh etc) - sometimes https
-  to the outside is not available too :(
-* Get a dummy user going for testing deployment
-* Add git 
+```sh
+./doit
+```
+
+
+### Others
+
+
+Pex generation:
+
+```sh
+pex --compile --python=python3 -r deploy/requirements.txt -m doit -o doit
+```
--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,9 @@
 #!/usr/bin/zsh
 
-# setup directories
-GITDIR_PREFIX='git'
+SCRIPT_PATH=$(readlink -f $0)
+REPO_DIR=`dirname $SCRIPT_PATH`
 
-dotfile_dir=`pwd`
-repo_dir=$dotfile_dir/..
-
-home=(
-    '.gitconfig'
-    '.gitignore-global'
-    '.config/i3'
-    '.config/polybar'
-    '.config/termite'
-    '.sh_aliases'
-    '.spacemacs'
-    '.tmux.conf'
-    '.zlogin'
-    '.zshenv'
-    '.zshrc'
-)
-
-gitdirs=('perso' 'work')
-
-echo 'Building directories...'
-for i in $gitdirs; do
-    mkdir -p "$HOME/$GITDIR_PREFIX/$i";
-done
-
-echo 'Deploying .files...'
-for i in $home; do
-    ln -nsf $dotfile_dir/$i ~/$i;
-    echo -e "\t$i deployed."
-done
-
-echo 'Installing .emacs.d...'
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
-# get tmux theme pack
-[ ! -d "$repo_dir/tmux-themepack" ] && git clone https://github.com/jimeh/tmux-themepack.git $repo_dir/tmux-themepack
-ln -nsf $repo_dir/tmux-themepack/ ~/.tmux-themepack
-
+echo "<Powerline fonts>"
 # patch fonts for oh-my-zsh
 read -sq $'PWL_CHOICE?Would you like to install Powerline fonts? [N/y]\n'
 if [[ "$PWL_CHOICE" = "y" ]]; then
@@ -48,6 +12,8 @@ if [[ "$PWL_CHOICE" = "y" ]]; then
     ([ -d "$repo_dir/fonts" ] && echo "Error in removing powerline fonts") || echo "Powerline fonts installed, don't forget to change shell fonts!"
 fi
 
-echo ''
+echo "<Dotfiles>"
+$REPO_DIR/doit deploy || exit 1
 
-cd ~ && source ~/.zshrc
+echo "<Source .zshrc>"
+source ~/.zshrc
